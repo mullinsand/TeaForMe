6a6aec6fe88a83ecb2faacd844156b83f82d68d2
Andrew Mullins <mullinsand15@gmail.com>
2023-01-09 20:18:59 -0700

    customer model validation logic

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ApplicationRecord
-  
+  validates_presence_of :first_name,
+                       :last_name,
+                       :email,
+                       :address
+  has_many :customers_subscriptions
+  has_many :subscriptions, through: :customers_subscriptions
 end
