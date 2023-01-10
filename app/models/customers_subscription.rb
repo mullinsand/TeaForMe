4922c4993ad021ae1705b8723556c6e80b24c444
Andrew Mullins <mullinsand15@gmail.com>
2023-01-09 20:24:16 -0700

    customers_subscription model validation and tests

--- app/models/customers_subscription.rb
+++ app/models/customers_subscription.rb
@@ -1,4 +1,6 @@
 class CustomersSubscription < ApplicationRecord
+  enum status: { active: 0, cancelled: 1 }
+
   belongs_to :customers
   belongs_to :subscriptions
 end
--- spec/models/customers_subscription_spec.rb
+++ spec/models/customers_subscription_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CustomersSubscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :customers }
+    it { should belong_to :subscriptions }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:status) }
+  end
 end
