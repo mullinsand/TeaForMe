2b8c204a38fe580c0e47cfad2f62e06364c126de
Andrew Mullins <mullinsand15@gmail.com>
2023-01-09 21:37:08 -0700

    update factories

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :subscription do
-    title { "MyString" }
-    price { 1 }
-    frequency { 1 }
+    title { Faker::Commerce.product_name }
+    price { Faker::Number.between(from: 100, to: 100000) }
+    frequency { Faker::Number.between(from: 1, to: 52) }
   end
 end
--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tea do
-    title { "MyString" }
-    description { "MyString" }
-    brew_time { "MyString" }
-    temperature { 1 }
+    title { Faker::Tea.variety }
+    description { Faker::Lorem.sentence }
+    brew_time { Faker::Number.between(from: 1, to: 20) }
+    temperature { Faker::Number.between(from: 100, to: 250) }
   end
 end
