ec5044009c1bfbb5a0e72c82d984c73be25fd807
Andrew Mullins <mullinsand15@gmail.com>
2023-01-09 20:47:24 -0700

    subscription model validation and tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:customers_subscriptions) }
+    it { should have_many(:customers).through(:customers_subscriptions) }
+    it { should have_many(:subscriptions_teas) }
+    it { should have_many(:teas).through(:subscriptions_teas) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:frequency) }
+  end
 end
