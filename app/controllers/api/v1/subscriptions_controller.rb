a0a02c5764e0590f5e1350c8a02909505c787c0c
Andrew Mullins <mullinsand15@gmail.com>
2023-01-10 13:55:06 -0700

    exception handler added to concerns

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  include ExceptionHandler
+  
   def index
     customer = Customer.find_by(api_key: params[:api_key])
     render json: SubscriptionSerializer.new(customer.subscriptions)
