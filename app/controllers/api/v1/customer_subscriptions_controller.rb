ea3967b86b84beefa96f4dda8c856bd196beb775
Andrew Mullins <mullinsand15@gmail.com>
2023-01-11 07:09:09 -0700

    already subscribed method add to controller with passing tests

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -6,11 +6,18 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
     raise ActiveRecord::RecordNotFound.new "Couldn't find customer with api key #{params[:api_key]}" if customer.nil?
 
     subscription = Subscription.find(params[:sub_id])
+    return render json: { message: "Customer already subscribed"} if already_subscribed?(customer)
     customer_sub = CustomersSubscription.new(customer_id: customer.id, subscription_id: subscription.id)
 
     if customer_sub.save
       render json: { message: "Customer successfully subscribed"}
     end
     #can't have duplicates: logic needed
+  end
+
+  private
+
+  def already_subscribed?(customer)
+    !customer.subscriptions.find_by(id: params[:sub_id]).nil?
   end
 end
